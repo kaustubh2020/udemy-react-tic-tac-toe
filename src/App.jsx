14eafb466f1b17962e5795199d00da206a7dfb0d
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-11-03 10:48:55 +0530

    Lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 const App = () => {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  const handleSelectSquare = () => {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  };
+
   return (
     <>
       <main>
         <div id="game-container">
-          <ol id="players">
-            <Player initialName="PLayer 1" symbol="X" />
-            <Player initialName="PLayer 2" symbol="O" />
+          <ol id="players" className="highlight-player">
+            <Player
+              initialName="PLayer 1"
+              symbol="X"
+              isActive={activePlayer === "X"}
+            />
+            <Player
+              initialName="PLayer 2"
+              symbol="O"
+              isActive={activePlayer === "O"}
+            />
           </ol>
-          <GameBoard />
+          <GameBoard
+            onSelectSquare={handleSelectSquare}
+            activePLayerSymbol={activePlayer}
+          />
         </div>
         LOG
       </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -22,7 +22,7 @@ const Player = ({ initialName, symbol }) => {
 
   return (
     <>
-      <li>
+      <li className={isActive ? "active" : undefined}>
         <span className="player">
           {editiblePlayerName}
           <span className="player-symbol">{symbol}</span>
